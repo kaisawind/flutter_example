60c46100ebabd66daba5221e4bc588f2a1efef5e
李佳春 <wind.kaisa@gmail.com>
2019-09-21 16:18:11 +0800

    跳过证书认证

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter_example/routers/routers.dart';
 import 'package:flutter_example/routers/application.dart';
 
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(MyApp());
 }
 
